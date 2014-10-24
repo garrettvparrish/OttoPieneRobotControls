dfe07e41901b627faccb916db0087a4549ea6884
garrett <garrett.v.parrish@gmail.com>
2014-10-24 12:45:00 -0400

    Added first commit.

---
+++ layout.js
@@ -0,0 +1,74 @@
+console.log("layout");
+
+var config = {
+    settings: {hasHeaders:false},
+    dimensions: {borderWidth:2},
+    content: [{
+        type: 'row',
+        content: [{
+            type: 'column',
+            width: 60,
+            content: [
+            	{
+	                type: 'component',
+	                componentName: 'graph',
+	                height: 100
+	            },{
+	                type: 'component',
+	                componentName: 'graph',
+	                height: 100
+	            },{
+	                type: 'component',
+	                componentName: 'graph',
+	                height: 100
+	            }
+            ]
+        }, {
+            type: 'column',
+            width: 60,
+            content: [
+            	{
+	                type: 'component',
+	                componentName: 'graph',
+	                height: 100
+	            },{
+	                type: 'component',
+	                componentName: 'graph',
+	                height: 100
+	            },{
+	                type: 'component',
+	                componentName: 'graph',
+	                height: 100
+	            }
+            ]
+
+        },{
+            type: 'column',
+            width: 60,
+            content: [
+            	{
+	                type: 'component',
+	                componentName: 'graph',
+	                height: 100
+	            },{
+	                type: 'component',
+	                componentName: 'graph',
+	                height: 100
+	            },{
+	                type: 'component',
+	                componentName: 'graph',
+	                height: 100
+	            }
+            ]
+
+        }]
+    }]
+};
+
+var myLayout = new GoldenLayout(config);
+
+myLayout.registerComponent('graph', function(container, state) {
+
+});
+
+myLayout.init();
