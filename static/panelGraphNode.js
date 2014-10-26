b4d9be56878ba35d5e46cc6e89cd1c920dec92b5
garrett <garrett.v.parrish@gmail.com>
2014-10-25 23:17:46 -0400

    Added more UI for graphs.

--- static/panelGraphNode.js
+++ static/panelGraphNode.js
@@ -4,110 +4,55 @@ var PanelGraphNode = function() {
 
   this.initPanel = function(container, componentState) {
 
-    // Create the graph
-    var graph = document.createElement('div');
-    graph.id = "graph-"+graphCount;
-    graph.className = "graph-container";
-    graphCount += 1;
+    var graphValue = document.createElement('div');
+    graphValue.id = "graph-" + graphCount + "-value";
+    graphValue.class = "graph-value";
+    graphValue.style.width = "100px";
+    graphValue.style.height = "100px";
 
+    // Create a title for the graph
     var title = "";
     switch (graphCount) {
       // Motor 1
-      case 1:
+      case 0:
         title = "MOTOR 1"
         break;
 
       // Motor 2
-      case 2:
+      case 1:
         title = "MOTOR 2"
         break;
 
       // Motor 3
-      case 3:
+      case 2:
         title = "MOTOR 3"
         break;
 
       // Rotational Velocity Graph
-      case 4:
+      case 3:
         title = "ROTATIONAL VELOCITY"
         break;
 
       // Horizontal Velocity Graph
-      case 5:
+      case 4:
         title = "HORIZONTAL VELOCITY"
         break;
 
       // Vertical Velocity Graph
-      case 6:
+      case 5:
         title = "VERTICAL VELOCITY"
         break;
 
       default:
         break;
     } 
+    console.log(graphValue);
 
-    container.getElement().html( '<h2 class="label">' + title + '</h2>' )
+    var heading = '<h2 class="label">' + title + '</h2>';
+    var value = '<h2 id="' + graphValue.id + '" class="graphValue">' + 0.0 + '</h2>'
+    container.getElement().html(heading + value);
 
-
-
-
-    container.getElement()[0].appendChild(graph);
-
-    // We use an inline data source in the example, usually data would
-    // be fetched from a server
-
-    var data = [],
-      totalPoints = 300;
-
-      if (data.length > 0)
-        data = data.slice(1);
-
-      // Do a random walk
-
-      while (data.length < totalPoints) {
-
-        var prev = data.length > 0 ? data[data.length - 1] : 50,
-          y = prev + Math.random() * 10 - 5;
-
-        if (y < 0) {
-          y = 0;
-        } else if (y > 100) {
-          y = 100;
-        }
-
-        data.push(y);
-      }
-
-      // Zip the generated y values with the x values
-
-      var res = [];
-      for (var i = 0; i < data.length; ++i) {
-        res.push([i, data[i]])
-      }
-
-      return res;
-    }
-
-    // Set up the control widget
-    var graphName = "#graph-"+graphCount;
-    // var plot = $.plot(graphName, [ getRandomData() ], {
-    //   series: {
-    //     shadowSize: 0 // Drawing is faster without shadows
-    //   },
-    //   yaxis: {
-    //     min: 0,
-    //     max: 100
-    //   },
-    //   xaxis: {
-    //     show: false
-    //   }
-    // });
-
-    function update() {
-      // plot.setData([getRandomData()]);
-      // plot.draw();
-      setTimeout(update, 1000);
-    }
-
-    update();
+    // Update graph count
+    graphCount += 1;
+  }
 }
