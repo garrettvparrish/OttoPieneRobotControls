1d4ef1aed63e04a21d259df498b387fd80e54b41
garrettp <garrettp@mit.edu>
2014-11-04 01:43:40 +0000

    Working socket connection.

--- Aaron/Socket_Test/socket.py
+++ Aaron/Socket_Test/socket.py
@@ -23,6 +23,9 @@ app = Flask(__name__, template_folder=tmpl_dir, static_url_path='')
 app.config['SECRET_KEY'] = 'lightballet'
 socketio = SocketIO(app)
 
+## CONFIGURE SERIAL CONNECTION
+port = serial.Serial("/dev/ttyAMA0", baudrate=19200, timeout=3.0)
+
 print "Initialized..."
 x = ''
 y = ''
@@ -39,10 +42,6 @@ m3 = 11
 # write to serial port
 def write(val):
     port.write(chr(val & 0xff))
-
-# configure motor controllers to determine baud rate
-#write(0xaa)
-##time.sleep(5)
 
 # dir : 0 = forward, 1 = backwards
 # id : motor number (128, 129, 130)
@@ -63,15 +62,18 @@ def updateValues(message):
     global x,y,r
     message = message['data']
     x,y,r = message.split(',')
+    _r = int(float(r) * 127)
 
+    # cap at zero
+    
     # only send commands to the motors if they are ready to receive
     # (handshake from the arduino has been processed)
-    if (READY):
-        # set motors to turn on (LEAAVE FOR NOW -- will just spin)
-        motorCommand(128, 1, r * 127)    
-        motorCommand(129, 1, r * 127)
-        motorCommand(130, 1, r * 127)
 
+    # set motors to turn on (LEAAVE FOR NOW -- will just spin)
+    motorCommand(128, 1, _r)
+    motorCommand(129, 1, _r)
+    motorCommand(130, 1, _r)
+    
     emit('motor1', r)
     emit('motor2', x)
     emit('motor3', y)
@@ -87,55 +89,3 @@ def disconnect():
 if __name__ == '__main__':
     socketio.run(app, host = '0.0.0.0')
 
-#############################################
-    
-#DEVICE = '/dev/ttyACM0'
-#BAUD = 9600
-#ser = serial.Serial(DEVICE, BAUD)
-
-#def takeGyroReading():
-#    gyroAngle =  ser.read()
-#    
-#    return int(gyroAngle)
-
-def translation(x,y):
-    motors = [1500,1500,1500]
-    
-    x = ((x + 1)*1000) / 2 + 1000 # map x between 1000 and 2000
-    y = ((y + 1)*1000) / 2 + 1000 # map y between 1000 and 2000
-
-    xMove = translateX(x)
-    yMove = translateY(y)
-    
-    for ii in range(3):
-        motors[ii] = (xMove[ii] + yMove[ii])/2
-    
-    return motors
-
-def translateX(x):
-    return [3000-x,3000-x,x]
-    
-def translateY(y):
-    return [y,y,1500] # 1 and 2 go full speed ahead and 3 does nothing
-
-def rotation(r):
-    #currentAngle = takeGyroReading()
-
-    
-    
-    r = ((r + 1)*1000) / 2 + 1000 # map r between 1000 and 2000
-    return [r, r, r]
-
-def smoothMotors(currentMotors):
-    global lastMotors
-
-    for ii in range(3):
-        if currentMotors[ii] < lastMotors[ii]:
-            currentMotors[ii] -= 1
-        elif currentMotors[ii] > lastMotors[ii]:
-            currentMotors[ii] += 1
-
-    lastMotors = currentMotors
-    return currentMotors
-
-## SET DIGITAL HIGH TO THE ARDUINO
